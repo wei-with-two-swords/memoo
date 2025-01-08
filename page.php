cf786a527116866f4e3fdffb7008b8e717fc128b
shuan <shuangjian_wei@163.com>
2025-01-08 09:25:56 +0800

    update page.php

--- page.php
+++ page.php
@@ -1,7 +1,7 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <?php $this->need('header.php'); ?>
 
-<div class="col-mb-12 col-8" id="main" role="main">
+<div class="col-sm-12 col-md-8" id="main" role="main">
     <article class="post" itemscope itemtype="http://schema.org/BlogPosting">
         <h1 class="post-title" itemprop="name headline">
             <a itemprop="url"
